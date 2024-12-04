613e44a700f2ffaffd26405dcf5985ee37e8e44e
muyanja <muyanjarodney266@gmail.com>
2024-12-04 11:06:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Text;
+
+var str = ValidString("a)b(c)d");
+Console.WriteLine(str);
+
+string ValidString(string str)
+{
+    StringBuilder sb = new ();
+
+    Stack<char> s = new ();
+    foreach (var c in str)
+    {
+        s.Push (c);
+    }
+
+    while(s.Peek() != ')')
+    {
+        if(s.Peek() == '(' && s.Peek() == '(')
+        {
+            continue;
+        }
+
+        sb.Append (s.Pop());    
+    }
+
+    sb.Append(s.Pop());
+    if(s.Peek() != ')')
+    {
+        while(s.Peek () != ')')
+        {
+            sb.Append (s.Pop());
+        }
+    }
+
+    return sb.ToString ();
+}
